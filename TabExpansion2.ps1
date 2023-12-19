4ebf89753bc0f46aef945925748f095f9962988d
94v1c8 <8792065+94v1c8@users.noreply.github.com>
2023-12-19 19:51:43 +1030

    commit

---
+++ TabExpansion2.ps1
@@ -0,0 +1,43 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0
+
+.GUID 38e86e4e-22a9-4443-93bf-3fd989dd7112
+
+.AUTHOR 94v1c8 <94v1c8@pcb.id.au>
+
+.COMPANYNAME Home of the Insane
+
+.COPYRIGHT 2023
+
+.TAGS
+
+.LICENSEURI https://github.com/94v1c8/powershell/LICENSE
+
+.PROJECTURI https://github.com/94v1c8
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+
+
+.PRIVATEDATA
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ Provides enhancements to the tab auto-completion in PowerShell. 
+
+#> 
+Param()
+
+
